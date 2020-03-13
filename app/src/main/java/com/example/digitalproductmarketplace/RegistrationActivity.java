46547cea804473b18b5112b6fd173187781b191c
kumarr12 <Raman@DESKTOP-79L34IT>
2020-03-13 16:25:06 -0700

    Changes in registration activity are commited

--- app/src/main/java/com/example/digitalproductmarketplace/RegistrationActivity.java
+++ app/src/main/java/com/example/digitalproductmarketplace/RegistrationActivity.java
@@ -3,6 +3,7 @@ package com.example.digitalproductmarketplace;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 public class RegistrationActivity extends AppCompatActivity {
 
@@ -10,5 +11,9 @@ public class RegistrationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+        ConstraintLayout registrationLayout = findViewById(R.id.registrationLayout);
+
+        //https://digitalsynopsis.com/design/beautiful-color-ui-gradients-backgrounds/
+        registrationLayout.setBackgroundResource(R.drawable.bluebg);
     }
 }
