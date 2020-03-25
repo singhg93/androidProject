884b1d19cbfc23cc53c582b936282d80217814b4
kumarr12 <Raman@DESKTOP-79L34IT>
2020-03-25 01:49:18 -0700

    made a menu item and changes add to profile activity with menu item layout

--- app/src/main/java/com/example/digitalproductmarketplace/ProfileActivity.java
+++ app/src/main/java/com/example/digitalproductmarketplace/ProfileActivity.java
@@ -1,11 +1,17 @@
 package com.example.digitalproductmarketplace;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.digitalproductmarketplace.Boundary.UserDAO;
@@ -18,6 +24,22 @@ public class ProfileActivity extends AppCompatActivity {
     TextView _firstName;
     TextView _lastName;
     TextView _email;
+    Button _logOut;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflator = getMenuInflater();
+        inflator.inflate(R.menu.logout_item_menu, menu);
+        return true;
+    }
+
+//    @Override
+//    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+//        switch (item.getItemId()){
+//            case R.id.logOutMenu:
+//
+//        }
+//    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +50,8 @@ public class ProfileActivity extends AppCompatActivity {
         _firstName = findViewById(R.id.profileFirstName);
         _lastName = findViewById(R.id.profileLastName);
         _email = findViewById(R.id.profileEmail);
+        _logOut = findViewById(R.id.logOutBtn);
+
 
         SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(this);
         if (sharedPref.contains("EMAIL")) {
@@ -49,5 +73,14 @@ public class ProfileActivity extends AppCompatActivity {
             _lastName.setText(_signedInUser.get_lastName());
             _email.setText(_signedInUser.get_email());
         }
+
+//        _logOut.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//
+//            }
+//        });
+
+
     }
 }
