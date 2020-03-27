c24d34e879c019bf11e621ee643e1b6a068fa868
singhg93 <singhg93@student.douglascollege.ca>
2020-03-27 12:49:39 -0700

    Added the code for creating posts table

--- app/src/main/java/com/example/digitalproductmarketplace/Boundary/DBHelper.java
+++ app/src/main/java/com/example/digitalproductmarketplace/Boundary/DBHelper.java
@@ -18,7 +18,7 @@ public class DBHelper extends SQLiteOpenHelper {
     public static final String PASSWORD_COLUMN = "password";
 
     // column names for items table
-    public static final String ITEMS_TABLE_NAME = "items;";
+    public static final String ITEMS_TABLE_NAME = "items";
     public static final String ITEM_ID = "id";
     public static final String DESCRIPTION = "description";
     public static final String PRICE = "price";
@@ -26,6 +26,14 @@ public class DBHelper extends SQLiteOpenHelper {
     public static final String USER_ID = "userId";
     public static final String PICTURE = "picName";
     public static final String FILE_URL = "fileUrl";
+
+    // column names for adPosts table
+    public static final String AD_POST_TABLE = "advertisements";
+    public static final String AD_ID = "id";
+    public static final String ITEM_ID_FK = "itemId";
+    public static final String DATE_POSTED = "datePosted";
+    public static final String DATE_UPDATED = "lastUpdated";
+
 
     public DBHelper( Context context) {
         super(context, DB_NAME, null, DB_VERSION, null);
@@ -48,16 +56,38 @@ public class DBHelper extends SQLiteOpenHelper {
                 + "( "
                 + ITEM_ID + " INTEGER PRIMARY KEY, "
                 + DESCRIPTION + " TEXT NOT NULL, "
-                + PRICE + " DOUBLE NOT NULL, "
+                + PRICE + " REAL NOT NULL, "
                 + CATEGORY +" TEXT NOT NULL, "
                 + USER_ID + " INTEGER NOT NULL,"
                 + PICTURE + " TEXT NOT NULL,"
-                + FILE_URL + " TEXT NOT NULL," +
-                "FOREIGN KEY " + USER_ID + " REFERENCES "
+                + FILE_URL + " TEXT NOT NULL,"
+                + "FOREIGN KEY (" + USER_ID + ") REFERENCES "
                 + USERS_TABLE_NAME + "(id)" +
                 ");";
 
         db.execSQL(sqlItemsCreate);
+//
+//        public static final String AD_POST_TABLE = "advertisements;";
+//        public static final String AD_ID = "id";
+//        public static final String ITEM_ID_FK = "itemId";
+//        public static final String DATE_POSTED = "datePosted";
+//        public static final String DATE_UPDATED = "lastUpdated";
+
+        String sqlAdsCreate = "CREATE TABLE " + AD_POST_TABLE
+                + "( "
+                + AD_ID + " INTEGER PRIMARY KEY, "
+                + ITEM_ID_FK + " INTEGER NOT NULL, "
+                + USER_ID + " INTEGER NOT NULL, "
+                + PRICE + " REAL NOT NULL, "
+                + DATE_POSTED +" INTEGER NOT NULL, "
+                + DATE_UPDATED + " INTEGER NOT NULL,"
+                + "FOREIGN KEY (" + USER_ID + ") REFERENCES "
+                + USERS_TABLE_NAME+ "(id),"
+                + "FOREIGN KEY (" + ITEM_ID_FK + ") REFERENCES "
+                + ITEMS_TABLE_NAME+ "(id)"
+                + ");";
+
+        db.execSQL(sqlAdsCreate);
 
     }
 
@@ -66,6 +96,7 @@ public class DBHelper extends SQLiteOpenHelper {
 
         db.execSQL("DROP TABLE IF EXISTS " + USERS_TABLE_NAME + ";");
         db.execSQL("DROP TABLE IF EXISTS " + ITEMS_TABLE_NAME + ";");
+        db.execSQL("DROP TABLE IF EXISTS " + AD_POST_TABLE + ";");
         onCreate(db);
     }
 }
