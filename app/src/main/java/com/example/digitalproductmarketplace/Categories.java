e3b57cdd466ac8f92ae5b96c452d7278cbadeb1e
singhg93 <singhg93@student.douglascollege.ca>
2020-03-27 14:31:13 -0700

    added dummy code for passing bundle

--- app/src/main/java/com/example/digitalproductmarketplace/Categories.java
+++ app/src/main/java/com/example/digitalproductmarketplace/Categories.java
@@ -1,6 +1,7 @@
 package com.example.digitalproductmarketplace;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -13,6 +14,12 @@ public class Categories extends AppCompatActivity {
     Button _videoCatBtn;
     Button _ebookCatBtn;
     Button _grapicCatBtn;
+
+    CardView _audioCard;
+    CardView _videoCard;
+    CardView _ebookCard;
+    CardView _graphicCard;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,6 +29,19 @@ public class Categories extends AppCompatActivity {
         _videoCatBtn = findViewById(R.id.ButtonVideo);
         _ebookCatBtn = findViewById(R.id.ButtonEbooks);
         _grapicCatBtn = findViewById(R.id.ButtonGraphics);
+
+        _audioCard = findViewById(R.id.audio_card);
+        _videoCard = findViewById(R.id.video_card);
+        _ebookCard = findViewById(R.id.video_card);
+        _graphicCard = findViewById(R.id.graphic_card);
+
+        _audioCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // create a bundle with key CATEGORY, and value audio
+                // do the same for other three cards
+            }
+        });
 
         //
     }
