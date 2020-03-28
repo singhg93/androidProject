ed1b0fea072a291c4801bc7c7d011b5c6e2c2e34
kumarr12 <Raman@DESKTOP-79L34IT>
2020-03-27 23:50:10 -0700

    category name in bundle as _audioCard and others too done and successful tested

--- app/src/main/java/com/example/digitalproductmarketplace/Categories.java
+++ app/src/main/java/com/example/digitalproductmarketplace/Categories.java
@@ -7,42 +7,71 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class Categories extends AppCompatActivity {
 
-    Button _audioCatBtn;
-    Button _videoCatBtn;
-    Button _ebookCatBtn;
-    Button _grapicCatBtn;
+    Button _audioCardBtn;
+    Button _videoCardBtn;
+    Button _ebookCardBtn;
+    Button _grapicCardBtn;
 
     CardView _audioCard;
     CardView _videoCard;
     CardView _ebookCard;
     CardView _graphicCard;
 
+    Intent itemIntent;
+    Bundle itemBundle = new Bundle();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_categories);
 
-        _audioCatBtn = findViewById(R.id.ButtonAudio);
-        _videoCatBtn = findViewById(R.id.ButtonVideo);
-        _ebookCatBtn = findViewById(R.id.ButtonEbooks);
-        _grapicCatBtn = findViewById(R.id.ButtonGraphics);
+        _audioCardBtn = findViewById(R.id.ButtonAudio);
+        _videoCardBtn = findViewById(R.id.ButtonVideo);
+        _ebookCardBtn = findViewById(R.id.ButtonEbooks);
+        _grapicCardBtn = findViewById(R.id.ButtonGraphics);
 
         _audioCard = findViewById(R.id.audio_card);
         _videoCard = findViewById(R.id.video_card);
         _ebookCard = findViewById(R.id.video_card);
         _graphicCard = findViewById(R.id.graphic_card);
 
-        _audioCard.setOnClickListener(new View.OnClickListener() {
+        _audioCardBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // create a bundle with key CATEGORY, and value audio
-                // do the same for other three cards
+                itemBundle.putString("CATEGORY","_audioCard");
+                startActivity();
+            }
+        });
+        _videoCardBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                itemBundle.putString("CATEGORY","_videoCard");
+                startActivity();
+            }
+        });
+        _ebookCardBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                itemBundle.putString("CATEGORY","_ebookCard");
+                startActivity();
+            }
+        });
+        _grapicCardBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                itemBundle.putString("CATEGORY","_graphicCard");
+                startActivity();
             }
         });
 
-        //
+    }
+
+    private void startActivity(){
+        itemIntent = new Intent(Categories.this, ItemsActivity.class);
+        itemIntent.putExtras(itemBundle);
+        startActivity(itemIntent);
     }
 }
