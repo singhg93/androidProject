9bcb3442e4ffde47c46bafe444dd62c5a2a470a6
singhg93 <singhg93@student.douglascollege.ca>
2020-04-03 19:14:55 -0700

    changed the app to only use items instead of advertisement posts

--- app/src/main/java/com/example/digitalproductmarketplace/ItemsActivity.java
+++ app/src/main/java/com/example/digitalproductmarketplace/ItemsActivity.java
@@ -1,22 +1,27 @@
 package com.example.digitalproductmarketplace;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
 import android.widget.TextView;
 import com.example.digitalproductmarketplace.boundary.AdvertisementDAO;
+import com.example.digitalproductmarketplace.boundary.ItemDAO;
 import com.example.digitalproductmarketplace.entity.AdvertisementPost;
+import com.example.digitalproductmarketplace.entity.Item;
 
 import java.util.ArrayList;
 
 public class ItemsActivity extends AppCompatActivity {
 
     RecyclerView recyclerViewData;
-    AdvertisementDAO advertisementDAO;
+    ItemDAO itemDAO;
     String category;
-    ArrayList<AdvertisementPost> advertisementPosts;
+    ArrayList<Item> items;
 
 
     @Override
@@ -37,7 +42,7 @@ public class ItemsActivity extends AppCompatActivity {
         }
 
         // initialize the advertisement dao
-        advertisementDAO = new AdvertisementDAO(getApplicationContext());
+        itemDAO = new ItemDAO(getApplicationContext());
 
         // get the recycler data
         recyclerViewData = findViewById(R.id.recyclerViewData);
@@ -45,19 +50,29 @@ public class ItemsActivity extends AppCompatActivity {
         // if category is not null
         if (category != null) {
             //  if the user wants to browse a particular category, get all the posts related to that category
-            advertisementPosts = advertisementDAO.getPostsForCategory(category);
+            items = itemDAO.getItemsForCategory(category);
         } else {
             // get all the posts regardless of the category
-            advertisementPosts = advertisementDAO.getAllAds();
+            items = itemDAO.getAllItems();
         }
 
         // initialize an adapter
-        RecyclerViewCustomAdapter myRecyclerAdapter = new RecyclerViewCustomAdapter(advertisementPosts);
+        RecyclerViewCustomAdapter myRecyclerAdapter = new RecyclerViewCustomAdapter(items, ItemsActivity.this);
 
         // set the a layoutManager
         recyclerViewData.setLayoutManager(new LinearLayoutManager(this));
 
         // set the adapte
         recyclerViewData.setAdapter(myRecyclerAdapter);
+
+
+
+
+//        recyclerViewData.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                startActivity(new Intent(ItemsActivity.this, MainActivity.class));
+//            }
+//        });
     }
 }
